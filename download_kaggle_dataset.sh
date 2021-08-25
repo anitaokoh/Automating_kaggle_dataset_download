e46aef72ec1450ebb50e17900ae37a4c9f91fdca
Anita Okoh <sleekymeeratura@gmail.com>
2021-08-25 07:14:45 +0000

    created a script to download kaggle dataset

---
+++ download_kaggle_dataset.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+#switch to root directory
+cd ..
+
+
+# set up kaggle api
+pip install kaggle
+mkdir .kaggle
+mv work/Automating_workflow/kaggle.json .kaggle
+
+
+# input the dataset api
+echo "Enter kaggle dataset API:"
+read dataset_api
+$dataset_api 
+
+
+#get unzipped data file
+unzip *.zip -d work/.
+
+
+#clean program
+rm *.zip && mv .kaggle/kaggle.json work/Automating_workflow/ && rmdir .kaggle 
+pip uninstall kaggle && cd work
+
+
+
+
